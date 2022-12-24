ff3470b2ab7a3a410b233794d2f3578ca6b13496
hajar tajayouti <tajayoutihajar@gmail.com>
2022-12-24 16:51:41 +0100

    addApDotNet

---
+++ DOT-NET-Product/MyProductsWebApi/MyProductsWebApi/Data/ProductsContext.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using MyProductWebApi.Models;
+
+namespace MyProductWebApi.Data
+{
+    public partial class ProductsContext : DbContext
+    {
+        public ProductsContext()
+        {
+        }
+
+        public ProductsContext(DbContextOptions<ProductsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Category> Categories { get; set; } = null!;
+        public virtual DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("utf8mb4_general_ci")
+                .HasCharSet("utf8mb4");
+
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.ToTable("categories");
+
+                entity.HasCharSet("utf8")
+                    .UseCollation("utf8_general_ci");
+
+                entity.Property(e => e.CategoryId)
+                    .HasColumnType("int(11)")
+                    .HasColumnName("CategoryID");
+
+                entity.Property(e => e.CategoryName).HasMaxLength(15);
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("products");
+
+                entity.HasCharSet("utf8")
+                    .UseCollation("utf8_general_ci");
+
+                entity.HasIndex(e => e.CategoryId, "FK_ProductsCategories");
+
+                entity.Property(e => e.ProductId)
+                    .HasColumnType("int(11)")
+                    .HasColumnName("ProductID");
+
+                entity.Property(e => e.CategoryId)
+                    .HasColumnType("int(11)")
+                    .HasColumnName("CategoryID");
+
+                entity.Property(e => e.ProductName).HasMaxLength(40);
+
+                entity.Property(e => e.UnitPrice)
+                    .HasPrecision(19, 4)
+                    .HasDefaultValueSql("'0.0000'");
+
+                entity.Property(e => e.UnitsInStock)
+                    .HasColumnType("int(11)")
+                    .HasDefaultValueSql("'0'");
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.Products)
+                    .HasForeignKey(d => d.CategoryId)
+                    .HasConstraintName("FK_ProductsCategories");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
